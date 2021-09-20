f3ba7041f084acc3f8e55e1cee2664ad5708a164
Mansi Padgelwar <74247233+mansipadgelwar@users.noreply.github.com>
2021-09-20 04:57:24 +0000

    refactor: code

--- index.js
+++ index.js
@@ -15,8 +15,9 @@ var topScore = [
       score : 7
     }
   ];
-
-	var questionOne = {
+  
+   var questionOne = 
+   {
     question: " Let's start off easy, who all are part of Tapu Sena? \n a. Ross and Will Colbert \n b. Tapu akela hi puri sena hai \n c. Tapu, Gogo, and Pinki \n d. Tapu, Sonu, Goli, Gogi, and Pinku. \n \n",
     correctOption: "d",
     answer: "Tapu, Sonu, Goli, Gogi, and Pinku are part of Tapu Sena."
@@ -26,11 +27,12 @@ var topScore = [
       correctOption: "c",
       answer: "Champaklal Jayantilal Gada is Jethalal's father, Bapu ji's, full name."
       }
-     var questionThree = {
+    var questionThree = 
+    {
         question : " Most evenings, the group gets together at a shop to discuss everything under the sun. What is the name of the shop? \n a. Abdul Miyan's Store \n b. Aao-Khao store.  \n c. All-In-One General Store \n d. General Store \n",
         correctOption: "c",
          answer:"Name of the shop is All-In-One General Store."
-         }      
+     }      
     var questionFour =
     {
       question: "Complete this line from the title song of the show:\n Problem toh hai sabke saath \n Bas ___ ki hai baat \n Taarak Mehta ka ooltah chashmah \n a. suljhane \n b. nazariye \n c. bhulane \n d. dekhne  \n ",
@@ -66,15 +68,15 @@ var topScore = [
       question: "Who among the following is not a member of Gokuldham society? \n a. Aatmaram Tukaram Bhide \n b. Roshan Singh Sodhi \n c. Tulsidas Parekh  \n d. Hansraj Hathi  \n ",
 		  correctOption : "c",
       answer: "Tulsidas Parekh is not a member of Gokuldham society. "
-    }  
-  var questionTen =
-  {
+    }
+    var questionTen =
+    {
     question: "Which of the following shows had a crossover with Taarak Mehta Ka Ooltah Chashmah? \n a. Kasuatti Zindagi Kay \n b. Bhabhiji Ghar Par Hain  \n c. C.I.D \n d. Sumit Sambhal Lega  \n ",
     correctOption : "c",
     answer: "C.I.D had a crossover with Taarak Mehta Ka Ooltah Chashmah."
     }
     
-  var questions = [questionOne, questionTwo,questionThree,questionFour,questionFive,questionSix,questionSeven,questionEight,questionNine,questionTen];
+var questions = [questionOne, questionTwo,questionThree,questionFour,questionFive,questionSix,questionSeven,questionEight,questionNine,questionTen];
 	
 
 var score = 0;
@@ -85,7 +87,8 @@ console.log(chalk.red.bgWhite.bold( "Welcome "+userName+" to THE TAARAK MEHTA KA
 
 console.log(chalk.red.bgWhite.bold("---------------QUIZ BEGINS---------------\n"));
 
-function play(question,correctOption,answer){
+function play(question,correctOption,answer)
+{
   console.log(chalk.white.bgMagenta.bold("\n-------------- Q U E S T I O N --------------\n"));
   var userAnswer = readlineSync.question(question);
   if(userAnswer.toLowerCase() === correctOption)
@@ -94,18 +97,16 @@ function play(question,correctOption,answer){
       score = score + 1;
   }
   else
-	{
+  {
     console.log(chalk.red.bold("You are wrong!\n"));
     score = score - 1;
     console.log(chalk.red.bgWhite.bold(answer + "\n"));
   }
-  console.log(chalk.yellow("Current score: "+ score));    
-		
-	}  
+  console.log(chalk.yellow("Current score: "+ score));
+}
 
-
-	for(var i=0; i<questions.length; i++)
-  {
+for(var i=0; i<questions.length; i++)
+{
     var currentQuestion = questions[i];
     play(currentQuestion.question, currentQuestion.correctOption,currentQuestion.answer);
     if(score === 2){
@@ -114,7 +115,7 @@ function play(question,correctOption,answer){
     if(score === 4 ){
       console.log('You successfully completed level 2 & level 3 begins\n\n'); 
     }
-	}
+}
  
 
 function topScorer()
